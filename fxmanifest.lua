55a34b8530125f7e61175c89c3ccae024587a39c
LapisLabz <115321970+KoopaCode@users.noreply.github.com>
2025-11-21 00:22:20 -0500

    formated ts wrong
    Co-Authored-By: Seethh <225827027+Seethcc@users.noreply.github.com>

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,11 +1,11 @@
-fx_version 'cerulean'
-games { 'gta5' }
-lua54 'yes'
-
 name 'Hades Recoil'
 description 'Recoil control script for FiveM'
 author 'HadesStudios'
 version '1.0.0'
+
+fx_version 'cerulean'
+games { 'gta5' }
+lua54 'yes'
 
 client_script "@hades-base/components/cl_error.lua"
 client_script "@hades-pwnzor/client/check.lua"
